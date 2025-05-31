0482b25b9f9d9a436d6b9f814e5a53ed13a06ee2
Jaehyeon2k <wmspfl2673@gmail.com>
2025-06-01 01:18:38 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 14.12 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String front = my_string.substring(0, s);
-        String back = my_string.substring(s + overwrite_string.length());
+        String front = my_string.substring(0, s); // front값은 my_string 문자열을 0부터 2 이전까지
+        String back = my_string.substring(s + overwrite_string.length()); // back값은 2 + 7 = 9이다. 즉, "d" 이다.
         return front + overwrite_string + back;
     }
 }
